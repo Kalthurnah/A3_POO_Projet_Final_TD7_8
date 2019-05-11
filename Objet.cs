edc4b754064ce3c5e46ff88a388c1d9134c07e9d
Kalthurnah <48201356+Kalthurnah@users.noreply.github.com>
2019-05-11 16:16:45 +0200

    Ajout de la classe objet
    J'ai pas trouvé comment faire le enum et je suis pas convacinu par le code qu'il convertitpar contre ... Je pensais faire un énum pour le type d'objet comme il y a en annexe du sujet

---
+++ Objet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TD7_8
+{
+    public abstract class Objet
+    {
+        private string référence;
+        private string type;
+        private string description;
+        private double prix;
+
+        public Objet()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Référence
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string Type
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public string Description
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int Prix
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
