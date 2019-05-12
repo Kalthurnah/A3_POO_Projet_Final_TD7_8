9c2377c2c5a2cacc62667d4ef2451bdf5890df00
Amine A <amine-98@live.fr>
2019-05-12 17:39:19 +0200

    Refonte de la classe ObjetVolumineux

---
+++ ObjetVolumineux.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TD7_8
+{
+    public abstract class ObjetVolumineux : Objet
+    {
+        public struct Dimension
+        {
+            public double longueur;
+            public double largeur;
+            public double hauteur;
+        }
+
+        private Dimension dimensions;
+
+        private double volume;
+
+
+        public Dimension Dimensions { get => dimensions; }
+        public double Volume { get => volume; }
+
+        /// <summary>
+        /// Constructeur d'un objet volumineux depuis ses dimensions données
+        /// </summary>
+        /// <param name="longueur">longueur de l'objet</param>
+        /// <param name="largeur">largeur de l'objet</param>
+        /// <param name="hauteur">hauteur de l'objet</param>
+        protected ObjetVolumineux(double longueur, double largeur, double hauteur)
+        {
+            dimensions.longueur = longueur;
+            dimensions.largeur = largeur;
+            dimensions.hauteur = hauteur;
+
+            MettreAJourVolume();//Mets à jour l'attribut volume selon les dimensions que l'on vient d'attribuer
+        }
+
+        /// <summary>
+        /// Méthode d'instance qui met à jour l'attribut du volume de l'objet depuis ses dimensions 
+        /// </summary>
+        private void MettreAJourVolume()
+        {
+            volume = CalculerVolume(dimensions);//On recalcule le volume de l'objet à partir de ses dimensions, et on met à jour l'attribut volume en conséquence.
+        }
+
+
+
+        /// <summary>
+        /// Méthode de classe calculant un volume depuis des dimensions
+        /// </summary>
+        /// <param name="dimensions">dimensions dont on veut calculer le volume</param>
+        /// <returns>Volume correspondant aux dimensions passées</returns>
+        private static double CalculerVolume(Dimension dimensions)
+        {
+            return dimensions.longueur * dimensions.largeur * dimensions.hauteur;
+        }
+    }
+}
