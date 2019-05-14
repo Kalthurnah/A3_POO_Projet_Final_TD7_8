57e50d6c604e737973a5019a9866b09d66f5d061
Amine A <amine-98@live.fr>
2019-05-14 23:42:56 +0200

    MAJ commentaires et disposition

--- Personnes/Personne.cs
+++ Personnes/Personne.cs
@@ -8,9 +8,11 @@ namespace TD7_8
 {
     public abstract class Personne : IIdentifiable
     {
-
-        static protected List<Personne> personnes = new List<Personne>();//Stocke les personne créees
+        static protected List<Personne> personnes = new List<Personne>();//Stocke les personne créees de facon statique dans la classe Personne.
         static int dernierIdDonne=0;// Le dernier id donné n'est pas forcément le nombre de personnes, dans le cas ou on en supprime par exemple.
+        public static int Count { get => CompterPersonnesTypees<Personne>(); }//Compteur de personne, qu'on remplace dans les classes filles avec new
+
+        //Attributs d'instance v .
 
         private int identifiant;
         protected string nom;
@@ -18,17 +20,16 @@ namespace TD7_8
         protected string adresse;
         protected string numeroTel;
 
+        public int Identifiant { get => identifiant; }
         /// <summary>
-        /// "Les différentes informations peuvent être modifiables", selon l'énoncé. Ci dessous get et set permettant cela.
+        /// "Les différentes informations peuvent être modifiables", selon l'énoncé. Ci dessous get et set permettant la modification, au besoin.
         /// </summary>
         public string Nom { get => nom; set => nom = value; }
         public string Prenom { get => prenom; set => prenom = value; }
         public string Adresse { get => adresse; set => adresse = value; }
         public string NumeroTel { get => numeroTel; set => numeroTel = value; }
 
-        public static int Count { get => CompterPersonnesTypees<Personne>(); }//Compteur de personne, qu'on remplace dans les classes filles avec new
 
-        public int Identifiant { get => identifiant; }
 
         /// <summary>
         /// Crée une instance de la classe Personne.
@@ -50,12 +51,12 @@ namespace TD7_8
 
 
         /// <summary>
-        /// Méthode protégée en interne, permettant de compter les personnes d'un type donné
-        /// Elle est appelée par les propriétés individuelles de compte, commme Personne.Compter ou Beneficiaire.Compter
+        /// Méthode statique protégée, permettant de compter les personnes d'un type donné.
+        /// Elle est appelée par les propriétés individuelles des classes, commme Personne.Count ou Beneficiaire.Count
         /// </summary>
         /// <typeparam name="T">Type de personnes qu'on veut compter</typeparam>
         /// <returns></returns>
-        protected static int CompterPersonnesTypees<T>() where T : Personne//Condition pour que cette méthode ne prenne que des types Personne.
+        protected static int CompterPersonnesTypees<T>() where T : Personne//Condition pour que cette méthode ne prenne que des types T héritant de Personne.
         {
             int compte = 0;
             foreach (T personneTypee in personnes.OfType<T>())
