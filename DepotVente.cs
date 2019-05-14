f7cb45ae120f41b6335985618adac9d241ad4894
Amine A <amine-98@live.fr>
2019-05-14 09:40:37 +0200

    Ajout du squelette de DepotVente

---
+++ DepotVente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TD7_8
+{
+    public class DepotVente : LieuStockage
+    {
+        //Todo Solde
+        //Todo Moyenne
+    }
+}
