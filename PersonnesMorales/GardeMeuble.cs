ffb86c3aad724914536090661e0c2071c4815e68
Amine A <amine-98@live.fr>
2019-05-17 17:18:04 +0200

    Retiré count garde meuble

--- PersonnesMorales/GardeMeuble.cs
+++ PersonnesMorales/GardeMeuble.cs
@@ -7,8 +7,6 @@ namespace TD7_8
 {
     public class GardeMeuble : LieuStockage
     {
-        public static int Count { get => lieuxStockage.OfType<GardeMeuble>().Count<GardeMeuble>(); }
-
         public GardeMeuble(string nom, string prenom, string adresse, string numeroTel) : base(nom, prenom, adresse, numeroTel)
         {
         }
