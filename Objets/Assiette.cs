c1c43be3f57097afb7e2646219fe8ae9583589a6
Kalthurnah <48201356+Kalthurnah@users.noreply.github.com>
2019-05-17 15:52:09 +0200

    Creation d'assiette et de couvert

--- Objets/Assiette.cs
+++ Objets/Assiette.cs
@@ -10,5 +10,28 @@ namespace TD7_8
         public Assiette(int nombreDePieces, double prix=0) : base(nombreDePieces,prix)
         {
         }
+
+        static public new Materiel InterfaceCreation()
+        {
+            bool valide = false;
+            int nombreDePieces = 0;
+            double prix = 0;
+            do
+            {
+                Dictionary<string, string> parametres = InteractionUtilisateur.DemanderParametres(new string[] { "nombre de pieces", "prix" });
+
+                try
+                {
+                    nombreDePieces = Convert.ToInt32(parametres["nombre de pieces"]);
+                    prix = Convert.ToDouble(parametres["largeur"]);
+                    valide = true;
+                }
+                catch
+                {
+                    valide = false;
+                }
+            } while (!valide);
+            return new Assiette(nombreDePieces, prix);
+        }
     }
 }
