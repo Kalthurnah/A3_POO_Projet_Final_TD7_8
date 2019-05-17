7e9afd4094720c500f09b7ad5e6d079f6b23ee91
Amine A <amine-98@live.fr>
2019-05-17 15:58:50 +0200

    Création Cuisinière

--- Objets/Cuisiniere.cs
+++ Objets/Cuisiniere.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace TD7_8
 {
-    public class Cuisiniere : ElectroMenager
+    public class Cuisiniere : ElectroMenager, ICreableInterface<object>
     {
         private int puissance;
         private int nombrePlaques;
@@ -18,5 +18,38 @@ namespace TD7_8
 
         public int NombrePlaques { get => nombrePlaques; }
         public int Puissance { get => puissance; }
+
+        static public new Materiel InterfaceCreation()
+        {
+            bool valid = false;
+            int nombrePlaques = 0;
+            int puissance = 0;
+            double longueur = 0;
+            double largeur = 0;
+            double hauteur = 0;
+            double prix = 0;
+            do
+            {
+                Dictionary<string, string> parametres = InteractionUtilisateur.DemanderParametres(new string[] { "puissance", "nombrePlaques", "longueur", "largeur", "hauteur", "prix" });
+
+                try
+                {
+                    puissance = Convert.ToInt32(parametres["puissance"]);
+                    nombrePlaques = Convert.ToInt32(parametres["nombrePlaques"]);
+                    longueur = Convert.ToDouble(parametres["longueur"]);
+                    longueur = Convert.ToDouble(parametres["longueur"]);
+                    largeur = Convert.ToDouble(parametres["largeur"]);
+                    hauteur = Convert.ToDouble(parametres["largeur"]);
+                    prix = Convert.ToDouble(parametres["largeur"]);
+                    valid = true;
+                }
+                catch
+                {
+                    valid = false;
+                }
+            } while (!valid);
+
+            return new Cuisiniere(puissance, nombrePlaques, longueur, largeur, hauteur, prix);
+        }
     }
 }
