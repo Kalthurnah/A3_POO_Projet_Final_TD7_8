1d6cb3a49df74bbdb0814e15afb3bdcf7bcd52b8
Amine A <amine-98@live.fr>
2019-05-17 10:11:15 +0200

    Menu & Interaction Utilisateurs

---
+++ Menu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD7_8
+{
+    public class Menu
+    {
+
+        public delegate void sousMenu();
+        public static sousMenu Rien = () => { };
+        public static void MenuRecherchePersonne()
+        {
+            int typeChoisi = InteractionUtilisateur.DemanderChoixInt("Choisir le type de personne à chercher :",
+                new string[] { "Adhérent", "Bénéficiaire", "Donateur", "Personne (Recherche globale)" });
+
+            switch (typeChoisi)
+            {
+                case 1:
+                    MenuRecherchePersonneMode<Adherent>();
+                    break;
+                case 2:
+                    MenuRecherchePersonneMode<Beneficiaire>();
+                    break;
+                case 3:
+                    MenuRecherchePersonneMode<Donateur>();
+                    break;
+                case 4:
+                    MenuRecherchePersonneMode<Personne>();
+                    break;
+            }
+        }
+        public static T MenuRecherchePersonneMode<T>(bool demanderChoix = false) where T : Personne
+        {
+            Recherche.FonctionRecherche<T> modeDeRechercheChoisi = InteractionUtilisateur.DemanderChoixObjet<Recherche.FonctionRecherche<T>>("Choisir le mode de recherche :",
+                new Recherche.FonctionRecherche<T>[] {
+                    Recherche.RechercheParNomPersonneTypee<T>,
+                    Recherche.RechercheParNumTelPersonneTypee<T>
+                },
+                new string[] { "Recherche par nom", "Recherche par numéro de téléphone" }
+                );
+            //on obtient la fonction de recherche correspondant au choix utilisateur
+
+
+            return InteractionUtilisateur.RechercherUnElement<T>(modeDeRechercheChoisi);
+        }
+        
+        public static void MenuDon()
+        {
+            //TODO
+
+            sousMenu menuChoisi = InteractionUtilisateur.DemanderChoixObjet<sousMenu>("Menu :",
+                 new sousMenu[] { Don.InterfaceValidationDons, Rien },
+                     new string[] { "Valider les dons en attente", "Quitter" });
+            menuChoisi();
+        }
+    }
+}
