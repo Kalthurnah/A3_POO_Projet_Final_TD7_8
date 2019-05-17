62049aa8ab74050f66c2f7d512d040a0a937d58c
Amine A <amine-98@live.fr>
2019-05-17 18:28:52 +0200

    Finalisation de l'interface creation Don

--- Don.cs
+++ Don.cs
@@ -163,19 +163,16 @@ namespace TD7_8
 
 
         //Todo com
-        public static Don InterfaceCreationDon()
+        public static Don InterfaceCreation()
         {
-            Materiel materiel = Materiel.InterfaceCreation();//TODO
-            Donateur donateur = InteractionUtilisateur.RechercherUnElement<Donateur>(Recherche.RechercheParNomPersonneTypee<Donateur>, demanderChoix: true, "nom");
-            Dictionary<string, string> parametres = InteractionUtilisateur.DemanderParametres(new string[] { "nom", "prenom", "adresse", "numeroTel" });
+            Materiel materiel = Materiel.InterfaceCreation();
+            Donateur donateur = Menu.MenuRecherchePersonneMode<Donateur>(demanderChoix: true);
             DateTime dateReception = InteractionUtilisateur.DemanderDateTime();
+
             Console.WriteLine("Entrez une description. (Ou laissez la vide)");
             string description = Console.ReadLine();
             Don don = new Don(materiel, donateur, dateReception, description);
-            //don = new Don(TODO);
-
             return don;
-
         }
     }
 
