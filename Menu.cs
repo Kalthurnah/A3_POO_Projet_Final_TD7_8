688fea25856b4fa796748c373aad6aee33585058
Amine A <amine-98@live.fr>
2019-05-19 01:16:13 +0200

    Check de div par 0

--- Menu.cs
+++ Menu.cs
@@ -175,7 +175,7 @@ namespace TD7_8
                      () => Console.WriteLine(Adherent.Count),
                      () => Console.WriteLine(Don.CountTraites<Materiel>()),
                      () => Console.WriteLine(Recherche.RechercheDonParStatutType<Materiel>("accepte").Count),
-                     () => Console.WriteLine(Don.CountTraites<ObjetVolumineux>(true,new Don.StatutDon[]{Don.StatutDon.Accepte, Don.StatutDon.Stocke})/Don.CountTraites<ObjetVolumineux>(true)),
+                     () => Console.WriteLine(Don.CountTraites<ObjetVolumineux>(true) == 0 ? 0 :Don.CountTraites<ObjetVolumineux>(true,new Don.StatutDon[]{Don.StatutDon.Accepte, Don.StatutDon.Stocke})/Don.CountTraites<ObjetVolumineux>(true)),
                      Tri.AfficherPrincipalesCategoriesEnStock,
                      Rien },
                      new string[] { "Obtenir la moyenne de temps entre la reception et le retrait des dons",
