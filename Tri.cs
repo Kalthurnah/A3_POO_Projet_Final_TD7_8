5e0bc431887cb628053b23aab2f76a253992c42c
Amine A <amine-98@live.fr>
2019-05-18 16:27:49 +0200

    Ajout d'une classe Tri vide (cf le commentaire dans la classe)

---
+++ Tri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD7_8
+{
+    class Tri
+    {
+        //Todo : faire des delegate de comparison, ou les créer dans leurs classes respectives
+        //https://docs.microsoft.com/en-us/dotnet/api/system.comparison-1?view=netframework-4.8
+        //Ex : Comparer don par identifiant, comparer don par nom, etc... J'pense que dans leurs classes elle meme ce serait mieux qu'ici 🤔
+    }
+}
